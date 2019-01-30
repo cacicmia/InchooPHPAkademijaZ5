22897e30890d9bc3ab5fb5583932f41b841ec752
cacicmia <cacic.miaa@gmail.com>
2019-01-30 14:59:32 +0100

    Initial commit

---
+++ app/controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mia
+ * Date: 30.01.19.
+ * Time: 14:56
+ */
+class IndexController
+{
+    public function index()
+    {
+        $view = new View()
+    }
+}
---
+++ app/model/App.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mia
+ * Date: 30.01.19.
+ * Time: 14:58
+ */
+final class App
+{
+    public static function start()
+    {
+        
+    }
+}
